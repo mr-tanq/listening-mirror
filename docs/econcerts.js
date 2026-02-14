67e370e6c6cd953ba09ca45de7108960250f4982
mr-tanq <errtanq9@gmail.com>
2026-02-14 21:36:05 +0100

    Update econcerts.js

--- docs/econcerts.js
+++ docs/econcerts.js
@@ -109,7 +109,7 @@
   const ECONCERTS_DEFAULTS = {
     size: 50,
     radiusKm: 70,       // only used if city is set
-    scoreMin: 40,       // ✅ cut weak matches by default
+    scoreMin: 50,       // ✅ cut weak matches by default
     tasteArtists: 1000,
     city: "Utrecht",           // empty => whole NL
     countryCode: "NL",

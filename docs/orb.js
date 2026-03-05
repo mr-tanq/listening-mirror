ff7eb0908ec3bb962be21caf35f689933e46af86
mr-tanq <errtanq9@gmail.com>
2026-03-05 03:13:33 +0100

    Update orb.js

--- docs/orb.js
+++ docs/orb.js
@@ -16,7 +16,7 @@
   const ANIM_FPS_CAP = 60;
 
   const MAX_DPR = 2.25;
-  const ORB_SIZE = 55;        // visual canvas size inside .glyph (CSS px)
+  const ORB_SIZE = 95;        // visual canvas size inside .glyph (CSS px)
   const HIT_PAD = 38;         // bigger tap target feel
 
   // Popup sizing

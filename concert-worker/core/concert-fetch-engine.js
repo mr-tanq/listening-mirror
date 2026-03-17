60c4b6cedadb9b8822101ae649574867b6c847f9
mr-tanq <errtanq9@gmail.com>
2026-03-17 08:21:51 +0100

    Update concert-fetch-engine.js

--- concert-worker/core/concert-fetch-engine.js
+++ concert-worker/core/concert-fetch-engine.js
@@ -1,1 +1,10 @@
+import { venueRegistry } from "./venue-registry.js";
 
+export async function fetchVenueEvents(source) {
+  const fn = venueRegistry[source];
+  if (!fn) throw new Error("unknown source");
+
+  const events = await fn();
+
+  return events;
+}

ba8630dd9f00d55caa6a1462ec81a81f08b466f1
mr-tanq <errtanq9@gmail.com>
2026-03-17 08:22:29 +0100

    Update concert-refresh.js

--- concert-worker/db/concert-refresh.js
+++ concert-worker/db/concert-refresh.js
@@ -1,1 +1,15 @@
+import { fetchVenueEvents } from "../core/concert-fetch-engine.js";
+import { deleteBySource, insertEvents } from "./concert-repository.js";
 
+export async function refreshSource(db, source) {
+  const events = await fetchVenueEvents(source);
+
+  await deleteBySource(db, source);
+
+  await insertEvents(db, events);
+
+  return {
+    source,
+    written: events.length
+  };
+}

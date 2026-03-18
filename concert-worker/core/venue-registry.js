fa6256c12aa6659304863049c447bab766177c05
mr-tanq <errtanq9@gmail.com>
2026-03-18 16:39:17 +0100

    Update venue-registry.js

--- concert-worker/core/venue-registry.js
+++ concert-worker/core/venue-registry.js
@@ -1,9 +1,11 @@
 import { fetchParadisoEvents } from "../parsers/parser-paradiso.js";
 import { fetchMelkwegEvents } from "../parsers/parser-melkweg.js";
 import { fetchTivoliEvents } from "../parsers/parser-tivoli.js";
+import { fetchPatronaatEvents } from "../parsers/parser-patronaat.js";
 
 export const venueRegistry = {
   paradiso: fetchParadisoEvents,
   melkweg: fetchMelkwegEvents,
-  tivoli: fetchTivoliEvents
+  tivoli: fetchTivoliEvents,
+  patronaat: fetchPatronaatEvents
 };

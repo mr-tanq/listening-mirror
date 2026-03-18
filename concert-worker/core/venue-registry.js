c6dbe5c176220eeb905dfc6478aea27acd04f03b
mr-tanq <errtanq9@gmail.com>
2026-03-18 16:34:17 +0100

    Update venue-registry.js

--- concert-worker/core/venue-registry.js
+++ concert-worker/core/venue-registry.js
@@ -1,5 +1,6 @@
 import { fetchParadisoEvents } from "../parsers/parser-paradiso.js";
 import { fetchMelkwegEvents } from "../parsers/parser-melkweg.js";
+import { fetchTivoliEvents } from "../parsers/parser-tivoli.js";
 
 export const venueRegistry = {
   paradiso: fetchParadisoEvents,

ab0a6a23f5d0e203c7498f001f9e17e8acc6aad5
mr-tanq <errtanq9@gmail.com>
2026-03-30 07:52:26 +0200

    Increase maxPages and adjust stopAfterEmptyPages

--- concert-worker/parsers/parser-melkweg.js
+++ concert-worker/parsers/parser-melkweg.js
@@ -5,8 +5,8 @@ const MELKWEG_SLUG = "Melkweg";
 
 export async function fetchMelkwegEvents(options = {}) {
   const {
-    maxPages = 20,
-    stopAfterEmptyPages = 3,
+    maxPages = 80,
+    stopAfterEmptyPages = 2,
     retriesPerPage = 2
   } = options;
 
